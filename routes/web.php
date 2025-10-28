96b962c3800759c6edcef69b4a31068cfb805bd4
Josue Castro <114679940+ItsBreo@users.noreply.github.com>
2025-10-28 20:06:36 +0000

    Login blade de ejemplo(sin controladores) con bootstrap y css

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta para mostrar el formulario de login
+Route::get('/login-local', function () {
+    return view('auth.login_local');
+})->name('login.local');

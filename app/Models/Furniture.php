9998f9eb4e6ffd27efbd20864ede91c3fad40c19
Josue Castro <114679940+ItsBreo@users.noreply.github.com>
2025-10-29 19:28:36 +0000

    solucion de errores

--- app/Models/Furniture.php
+++ app/Models/Furniture.php
@@ -107,18 +107,17 @@ class Furniture implements JsonSerializable {
     // Metodo datos mockup
     public static function furnitureData(): array{
         return[
-            new Furniture(1, 1, "Mueble 1", "Descripcion 1", 45, 3, "Madera de roble oscuro", "70cm x 140cm", "marrón oscuro de roble oscuro", true, "imagen.jpg");
-            new Furniture(2, 1, "Mueble 2", "Descripcion 2", 25, 7, "Madera refinada", "70cm x 140cm", "marrón oscuro de roble oscuro", false, "imagen2.jpg");
-            new Furniture(3, 1, "Mueble 3", "Descripcion 3", 79.99, 19, "Madera no refinada", "70cm x 140cm", "marrón oscuro de roble oscuro", false, "imagen3.jpg");
-            new Furniture(4, 2, "Mueble 4", "Descripcion 4", 15, 0, "Madera obtusa", "70cm x 140cm", "marrón oscuro de roble oscuro", true, "imagen4.jpg");
-            new Furniture(5, 3, "Mueble 5", "Descripcion 5", 10.20, 1, "Madera californiana", "45cm x 20cm", "marrón oscuro de roble oscuro", true, "imagen4.jpg");
-            new Furniture(6, 3, "Mueble 6", "Descripcion 6", 75, 6, "Toco Madera", "70cm x 180cm", "marrón oscuro de roble oscuro", false, "imagen5.jpg");
-            new Furniture(7, 4, "Mueble 7", "Descripcion 7", 35.55, 5, "Madera de Saturno", "50cm x 70cm", "marrón oscuro de roble oscuro", true, "imagen6.jpg");
-            new Furniture(8, 4, "Mueble 8", "Descripcion 8", 60, 3, "Madera de roble claro", "60cm x 100cm", "marrón oscuro de roble oscuro", true, "imagen7.jpg");
-            new Furniture(9, 4, "Mueble 9", "Descripcion 9", 69.99, 5, "Madera de Pino", "50cm x 120cm", "marrón oscuro de roble oscuro", false, "imagen8.jpg");
-            new Furniture(10, 4, "Mueble 10", "Descripcion 10", 7.99, 8, "Madera simple", "10cm x 45cm", "marrón oscuro de roble oscuro", false, "imagen9.jpg");
-
-        ]
+            new Furniture(1, 1, "Mueble 1", "Descripcion 1", 45, 3, "Madera de roble oscuro", "70cm x 140cm", "marrón oscuro de roble oscuro", true, "imagen.jpg"),
+            new Furniture(2, 1, "Mueble 2", "Descripcion 2", 25, 7, "Madera refinada", "70cm x 140cm", "marrón oscuro de roble oscuro", false, "imagen2.jpg"),
+            new Furniture(3, 1, "Mueble 3", "Descripcion 3", 79.99, 19, "Madera no refinada", "70cm x 140cm", "marrón oscuro de roble oscuro", false, "imagen3.jpg"),
+            new Furniture(4, 2, "Mueble 4", "Descripcion 4", 15, 0, "Madera obtusa", "70cm x 140cm", "marrón oscuro de roble oscuro", true, "imagen4.jpg"),
+            new Furniture(5, 3, "Mueble 5", "Descripcion 5", 10.20, 1, "Madera californiana", "45cm x 20cm", "marrón oscuro de roble oscuro", true, "imagen4.jpg"),
+            new Furniture(6, 3, "Mueble 6", "Descripcion 6", 75, 6, "Toco Madera", "70cm x 180cm", "marrón oscuro de roble oscuro", false, "imagen5.jpg"),
+            new Furniture(7, 4, "Mueble 7", "Descripcion 7", 35.55, 5, "Madera de Saturno", "50cm x 70cm", "marrón oscuro de roble oscuro", true, "imagen6.jpg"),
+            new Furniture(8, 4, "Mueble 8", "Descripcion 8", 60, 3, "Madera de roble claro", "60cm x 100cm", "marrón oscuro de roble oscuro", true, "imagen7.jpg"),
+            new Furniture(9, 4, "Mueble 9", "Descripcion 9", 69.99, 5, "Madera de Pino", "50cm x 120cm", "marrón oscuro de roble oscuro", false, "imagen8.jpg"),
+            new Furniture(10, 4, "Mueble 10", "Descripcion 10", 7.99, 8, "Madera simple", "10cm x 45cm", "marrón oscuro de roble oscuro", false, "imagen9.jpg"),
+        ];
     }
 
     // Buscar por categoria (-- Cambiar --)

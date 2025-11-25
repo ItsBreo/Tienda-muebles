c7faa2490655aab5ed43ecc49cd437b45dd29aa7
AzaelReyesMartel-1ASIR <azaelrm12@gmail.com>
2025-11-25 19:03:37 +0000

    Commit Categoria Seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,10 +10,10 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         // Usamos firstOrCreate para evitar duplicados si se ejecuta varias veces.
-        Category::firstOrCreate(['name' => 'Categoria 1']);
-        Category::firstOrCreate(['name' => 'Categoria 2']);
-        Category::firstOrCreate(['name' => 'Categoria 3']);
-        Category::firstOrCreate(['name' => 'Categoria 4']);
-        Category::firstOrCreate(['name' => 'Categoria 5']);
+        Category::firstOrCreate(['name' => 'Categoria 1', 'descripcion' => 'Descripcion 1']);
+        Category::firstOrCreate(['name' => 'Categoria 2', 'descripcion' => 'Descripcion 2']);
+        Category::firstOrCreate(['name' => 'Categoria 3', 'descripcion' => 'Descripcion 3']);
+        Category::firstOrCreate(['name' => 'Categoria 4', 'descripcion' => 'Descripcion 4']);
+        Category::firstOrCreate(['name' => 'Categoria 5', 'descripcion' => 'Descripcion 5']);
     }
 }

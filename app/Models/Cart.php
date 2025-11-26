3b65cabb9fda8812f6141701906163dfeac87f7d
JoseAAN <joseantonio.alonsonavarro@gmail.com>
2025-11-26 18:08:15 +0000

    Implementación de CartFactory y CartSeeder y del modelo Cart. Ya se generan carritos mediante el seeder

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cart extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'total'];
+    protected $fillable = ['user_id', 'total_price'];
 
     public function productos()
     {
--- database/seeders/CartSeeder.php
+++ database/seeders/CartSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Cart;
 use Illuminate\Database\Seeder;
 
 class CartSeeder extends Seeder
@@ -11,8 +11,8 @@ class CartSeeder extends Seeder
      * Run the database seeds.
      * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Cart::factory()->count(5)->create();
     }
 }
